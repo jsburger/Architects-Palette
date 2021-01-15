178f14a7c2d087bba45be261cbe8236352b72fee
jsburg <dizzydooble@gmail.com>
2021-01-15 04:55:44 -0500

    Added Entwine Sounds
    *Made Ender Pearl Block use Shroomlight sounds
    *Added Jub's new Entwine sounds

--- src/main/java/architectspalette/common/APBlockProperties.java
+++ src/main/java/architectspalette/common/APBlockProperties.java
@@ -3,6 +3,7 @@ package architectspalette.common;
 import architectspalette.common.blocks.CageLanternBlock;
 import architectspalette.common.blocks.abyssaline.AbyssalineHelper;
 import architectspalette.common.blocks.abyssaline.ChiseledAbyssalineBlock;
+import architectspalette.core.registry.APSounds;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
@@ -26,8 +27,8 @@ public class APBlockProperties {
 	 // As Nether Bricks
 	public static final Block.Properties ALGAL_BRICK = Block.Properties.create(Material.ROCK, MaterialColor.CYAN_TERRACOTTA).hardnessAndResistance(2.0F, 6.0F).sound(SoundType.NETHER_BRICK);
 
-	public static final Block.Properties ENTWINE = Block.Properties.create(Material.ROCK, MaterialColor.CYAN).hardnessAndResistance(3.0F, 6.0F)/*.sound(APSounds.APSoundTypes.ENTWINE)*/;
-	public static final Block.Properties ENDER_PEARL = Block.Properties.create(Material.ROCK, MaterialColor.CYAN).hardnessAndResistance(1.5F);
+	public static final Block.Properties ENTWINE = Block.Properties.create(Material.ROCK, MaterialColor.CYAN).hardnessAndResistance(3.0F, 6.0F).sound(APSounds.APSoundTypes.ENTWINE);
+	public static final Block.Properties ENDER_PEARL = Block.Properties.create(Material.ROCK, MaterialColor.CYAN).hardnessAndResistance(1.5F).sound(SoundType.SHROOMLIGHT);
 	public static final AbstractBlock.Properties PLATING = AbstractBlock.Properties.create(Material.IRON, MaterialColor.STONE).hardnessAndResistance(4.0F, 10.0F).sound(SoundType.NETHERITE);
 
 	public static final AbstractBlock.Properties MOLTEN_BRICK = Block.Properties.create(Material.ROCK, MaterialColor.NETHERRACK)
